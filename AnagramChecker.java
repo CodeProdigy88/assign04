bbbb7517526a6df060e0b11ba3484a0fc7a39bbb
CodeProdigy88 <cameronmckay88@gmail.com>
2026-06-02 19:03:34 -0600

    Add files via upload

--- AnagramChecker.java
+++ AnagramChecker.java
@@ -1,15 +1,39 @@
 package assign04;
 
 import java.util.Comparator;
-import java.util.Map;
 
 public class AnagramChecker {
 
+	/**
+	 * Sorts input lexicographically, ignoring captials
+	 * 
+	 * @param input - string to be sorted
+	 * @return lexicographically sorted string
+	 */
 	public static String sort(String input) {
-		char[] charArray = input.toCharArray();
-		return insertionSort(charArray, Comparator<? super T> cmp);
+
+		Character[] charArray = new Character[input.length()];
+		String lowerCase = input.toLowerCase();
+		char[] chars = lowerCase.toCharArray();
+
+		for (int i = 0; i < input.length(); i++) {
+			charArray[i] = chars[i];
+		}
+		Comparator<Character> myComparator = (s1, s2) -> s1.compareTo(s2);
+		insertionSort(charArray, myComparator);
+		String newArray = "";
+		for (int i = 0; i < charArray.length; i++) {
+			newArray += charArray[i];
+		}
+		return newArray;
 	}
 
+	/**
+	 * Uses insertion sort to sort an array
+	 * 
+	 * @param unsortArray - the unsorted array to be sorted
+	 * @param cmp         - The comparator to be used for sorting
+	 */
 	public static <T> void insertionSort(T[] unsortArray, Comparator<? super T> cmp) {
 		for (int i = 1; i < unsortArray.length; i++) {
 			T key = unsortArray[i];
@@ -23,11 +47,35 @@ public class AnagramChecker {
 		}
 	}
 
+	/**
+	 * Checks if 2 strings are anagrams
+	 * 
+	 * @param first   - The first word to be compared
+	 * @param second- The second word to be compared
+	 * @return - true if strings are anagrams, false if not
+	 */
 	public static boolean areAnagrams(String first, String second) {
-		return false;
+		return sort(first).equals(sort(second));
 	}
 
 	public static String[] getLargestAnagramGroup(String[] largestGroup) {
+		String[] sortedArray = new String[largestGroup.length];
+		for (int i = 0; i < largestGroup.length; i++) {
+			sortedArray[i] = sort(largestGroup[i]);
+		}
+		// Finds the highest amount in a row
+		int highestAmount = 0;
+		int amount = 0;
+		String anagramCurrent = "";
+		for (int i = 1; i < sortedArray.length; i++) {
+			if (sortedArray[i].compareTo(sortedArray[i - 1]) == 0) {
+				amount++;
+			}
+			if (amount > highestAmount) {
+				highestAmount = amount;
+				anagramCurrent = sortedArray[i];
+			}
+		}
 		return null;
 	}
 
--- AnagramCheckerTest.java
+++ AnagramCheckerTest.java
@@ -1,5 +1,58 @@
 package assign04;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Comparator;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * This class contains tests for UofUStudent.
+ * 
+ * @author Cameron McKay and Daler Turyssov
+ * @version 2025-05-15
+ */
+
 public class AnagramCheckerTest {
+
+	@Test
+	public void testAreAnagrams() {
+		assertTrue(AnagramChecker.areAnagrams("Same", "Same"));
+	}
+
+	@Test
+	public void testAreNotAnagrams() {
+		assertFalse(AnagramChecker.areAnagrams("Same", "NotSame"));
+	}
+
+	@Test
+	public void testActualAnagrams() {
+		assertTrue(AnagramChecker.areAnagrams("cat", "act"));
+	}
+
+	@Test
+	public void testActualAnagramsUpperCase() {
+		assertTrue(AnagramChecker.areAnagrams("Cat", "act"));
+	}
+
+	@Test
+	public void testInsertionSort() {
+		String words[] = { "june", "july", "august" };
+		Comparator<String> myComparator = (s1, s2) -> s1.compareTo(s2);
+		AnagramChecker.insertionSort(words, myComparator);
+		String[] excpectedWords = { "august", "july", "june" };
+		assertArrayEquals(excpectedWords, words);
+	}
+
+	@Test
+	public void testInsertionSortIntegers() {
+		Integer numbers[] = { 5, 7, 3 };
+		Comparator<Integer> myComparator = (s1, s2) -> s1.compareTo(s2);
+		AnagramChecker.insertionSort(numbers, myComparator);
+		Integer[] excpectedWords = { 3, 5, 7 };
+		assertArrayEquals(excpectedWords, numbers);
+	}
 
 }
